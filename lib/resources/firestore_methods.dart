d282141a0e3d2d429e170e886efce5429e555934
Shailendra <dagger.github@gmail.com>
2023-05-22 00:14:16 +0530

    Completed all the post screen UI and functions

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,36 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social_prac/models/post.dart';
+import 'package:social_prac/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //uploading post
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    String res = "Some error occured";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+      String postId = const Uuid().v1();
+
+      Post post = Post(
+          description: description,
+          uid: uid,
+          username: username,
+          postId: postId,
+          datePublished: DateTime.now(),
+          postUrl: photoUrl,
+          profImage: profImage,
+          likes: []);
+    } catch (err) {}
+  }
+}
